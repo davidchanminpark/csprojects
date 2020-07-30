1acf96901178838ccc077889ecb4a4e21b7a2848
David Chanmin Park <54333565+davidchanminpark@users.noreply.github.com>
2020-07-29 22:30:51 -0500

    Delete loader .h

--- C projects/LC4 Simulator/loader .h
+++
@@ -1,9 +0,0 @@
-/*
- * loader.h: Declares loader functions for opening and loading object files
- */
-
-#include <stdio.h>
-#include "LC4.h"
-
-// Read an object file and modify the machine state as described in the writeup
-int ReadObjectFile(char* filename, MachineState* CPU);
